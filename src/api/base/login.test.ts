fe369fbdbb264fca5f9b0f88ee37f4045bdc0964
GabelM <matthias.gabel@spiratec.com>
2023-12-07 11:47:21 +0100

    setup vitest

---
+++ src/api/base/login.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it } from 'vitest'
+import { Credentials } from '../models'
+import { login } from './login'
+
+describe('login function', () => {
+  it('should throw an error with wrong credentials', async () => {
+    const wrongCredentials: Credentials = {
+      username: 'wrongUsername',
+      password: 'wrongPassword'
+    }
+
+    await expect(login(wrongCredentials)).rejects.toThrow()
+  })
+})
