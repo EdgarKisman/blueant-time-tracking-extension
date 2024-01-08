c84ab0eb160e850674c8dd45b51011bbfd1a9b75
GabelM <matthias.gabel@spiratec.com>
2024-01-08 08:38:08 +0100

    setup .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
+import type { UserConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/services': 'http://vspe040-test'
+export default ({ mode }: { mode: string }): UserConfig => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/services': process.env.VITE_DOMAIN
+      }
     }
-  }
-})
+  })
+}
