36a51be4d46a264c388d7b5b78d9c11a05942965
GabelM <matthias.gabel@spiratec.com>
2024-01-08 08:38:34 +0100

    setup .env file

--- src/main.tsx
+++ src/main.tsx
@@ -12,5 +12,5 @@ if (rootElement !== null && rootElement !== undefined) {
     </React.StrictMode>
   )
 } else {
-  console.error("Root element not found")
+  console.error('Root element not found')
 }
