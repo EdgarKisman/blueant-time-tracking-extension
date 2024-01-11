83082a4a79b3fc83c2aca00dddd1d37e4a3eecaa
GabelM <matthias.gabel@spiratec.com>
2024-01-11 13:08:52 +0100

    processed PR

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,10 @@
-import { afterEach, describe, expect, it } from 'vitest'
-import { render, screen, cleanup } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+import { render } from '@testing-library/react'
 import App from './App'
 
 describe('Testing App Page', () => {
-  afterEach(cleanup)
   it('should show title', () => {
-    render(<App />)
-
-    expect(screen.getByText('BlueAnt Time Tracker')).toBeDefined()
+    const {getByText} = render(<App />)
+    expect(getByText('BlueAnt Time Tracker')).toBeDefined()
   })
 })
